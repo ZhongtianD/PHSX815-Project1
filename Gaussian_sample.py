2f980d2b1a0eff6e43023b37675a529987282c2c
Zhongtian Dong <78445813+ZhongtianD@users.noreply.github.com>
2021-02-20 21:22:47 -0600

    Add files via upload

---
+++ Gaussian_sample.py
@@ -0,0 +1,66 @@
+
+import sys
+import numpy as np
+
+
+sys.path.append(".")
+from Random import Gaussian
+
+# main function for our coin toss Python code
+if __name__ == "__main__":
+
+    # default seed
+    seed = 2048
+
+    # default mean of the distribution
+    mu = 0
+    
+    # default deviation of the distribution
+    sigma = 0
+    
+
+    # default number of samples (per experiment)
+    Nsample = 1
+
+    # default number of experiments
+    Nexp = 1
+
+    # output file defaults
+    doOutputFile = False
+
+    # read the user-provided seed from the command line (if there)
+    if '-seed' in sys.argv:
+        p = sys.argv.index('-seed')
+        seed = sys.argv[p+1]
+    if '-mu' in sys.argv:
+        p = sys.argv.index('-mu')
+        mu = float(sys.argv[p+1])
+    if '-sigma' in sys.argv:
+        p = sys.argv.index('-sigma')
+        ptemp = float(sys.argv[p+1])
+        if ptemp >= 0 :
+            sigma = ptemp
+    if '-Nsample' in sys.argv:
+        p = sys.argv.index('-Nsample')
+        Ns = int(sys.argv[p+1])
+        if Ns > 0:
+            Nsample = Ns
+    if '-Nexp' in sys.argv:
+        p = sys.argv.index('-Nexp')
+        Ne = int(sys.argv[p+1])
+        if Ne > 0:
+            Nexp = Ne
+    if '-output' in sys.argv:
+        p = sys.argv.index('-output')
+        OutputFileName = sys.argv[p+1]
+        doOutputFile = True
+
+    # class instance of our Random class using seed
+    G = Gaussian(seed =seed, mu=mu, sigma =sigma)
+
+    if doOutputFile:
+        S = np.zeros((Nexp,Nsample))
+        for e in range(Nexp):
+            S[e] = G.Gaussian_sample(mu,Nsample)
+        np.save(outfile, S)
+   
